50f3665d7c90691f953742c6ef0b4e0ba9ad27af
kengarshridhar <kengarshridhar007@outlook.com>
2025-05-01 01:44:54 +0530

    Del: Remove Console log

--- src/components/products/productsCard.tsx
+++ src/components/products/productsCard.tsx
@@ -1,8 +1,6 @@
 
 
 const ProductCard = ({ product } : { product: any }) => {
-    console.log("Product info")
-    console.log(product)
     return (
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
